6d996747e8e2cb7b41ab3fc7fc9bbae0f45d54c4
MOURSELAS Sokratis <smoursel@intrasoft-intl.com>
2019-06-11 09:47:47 +0300

    App.js change

--- App.js
+++ App.js
@@ -1,15 +1,8 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import ListItem from './src/components/ListItem/ListItem';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 export default class App extends Component{
 
